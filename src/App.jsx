b0e31822abded2d93dea11659cf31b6e63371392
Sanja <74556703+SanjaRogovic@users.noreply.github.com>
2023-09-11 23:21:54 +0200

    work in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import React, {useState} from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+const [data, setData] = useState({})
+const [loading, setLoading] = useState(false)
+const [location, setLocation] = useState("")
+
+
+const searchLocation = async () => {
+  try {
+    setLoading(true);
+    const key = import.meta.env.WEATHER_API_KEY;
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}`
+    );
+
+    console.log(response);
+
+    setData(response);
+  } catch (error) {
+    console.error(`request has failed with status: ${response.status}`);
+  } finally {
+    setLoading(false);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    searchLocation();
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {loading ? (
+        <div>
+          <div className="container">
+            <h1 className="header">Weather App</h1>
+          </div>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Search city"
+              value={input.value}
+              onChange={(e) => setLocation(e.target.value)}
+              required
+            />
+            <button className="btn" type="submit">
+              Search
+            </button>
+          </form>
+          <div className="temp">
+            <div>Feels like</div>
+          </div>
+          <div className="wind">
+            <div>Wind speed</div>
+          </div>
+          <div className="humid">
+            <div>Humidity</div>
+          </div>
+        </div>
+      ) : null}
     </>
-  )
+  );
 }
+}
+
+
+
 
 export default App
