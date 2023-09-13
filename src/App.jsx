cf4dd9d95720c161e1e990b964fcf709c34851ad
Sanja <74556703+SanjaRogovic@users.noreply.github.com>
2023-09-13 17:18:04 +0200

    adding some style

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,64 @@
-import './App.css'
-import React, {useState} from 'react'
+import React from 'react'
+import hotImg from "./assets/hot.jpg"
+import coldImg from "./assets/cold.jpg"
+import TempGrid from './components/TempGrid'
 
 function App() {
 
-const [data, setData] = useState({})
-const [loading, setLoading] = useState(false)
-const [location, setLocation] = useState("")
+// const [data, setData] = useState({})
+// const [loading, setLoading] = useState(false)
+// const [location, setLocation] = useState("")
 
 
-const searchLocation = async () => {
-  try {
-    setLoading(true);
-    const key = import.meta.env.WEATHER_API_KEY;
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}`
-    );
+// const searchLocation = async () => {
+//   try {
+//     setLoading(true);
+//     const key = import.meta.env.WEATHER_API_KEY;
+//     const response = await fetch(
+//       `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}`
+//     );
 
-    console.log(response);
+//     console.log(response);
 
-    setData(response);
-  } catch (error) {
-    console.error(`request has failed with status: ${response.status}`);
-  } finally {
-    setLoading(false);
-  }
+//     setData(response);
+//   } catch (error) {
+//     console.error(`request has failed with status: ${response.status}`);
+//   } finally {
+//     setLoading(false);
+//   }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    searchLocation();
-  };
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     searchLocation();
+//   };
 
   return (
     <>
-      {loading ? (
+    <div className="app" style={{backgroundImage: `url(${coldImg})`}}>
+    <div className="overlay">
+      <div className="container">
+        <div className="section section_input">
+          <input type="text" name='city' placeholder='Enter City Name'/>
+          <button>°F</button>
+        </div>
+        <div className="section section_temperature">
+          <div className="icon">
+            <h3>London, GB</h3>
+            <img src="" alt="weather icon" />
+            <h3>Cloudy</h3>
+          </div>
+          <div className="temperature">
+            <h1>34 °C</h1>
+          </div>
+        </div>
+        {/* bottom description */}
+        <TempGrid />
+
+      </div>
+    </div>
+      
+    </div>
+      {/* {loading ? (
         <div>
           <div className="container">
             <h1 className="header">Weather App</h1>
@@ -59,13 +85,9 @@ const searchLocation = async () => {
             <div>Humidity</div>
           </div>
         </div>
-      ) : null}
+      ) : null} */}
     </>
   );
 }
-}
-
-
-
 
 export default App
---
+++ src/components/TempGrid.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import "./tempgrid.css"
+import {FaArrowDown} from "react-icons/fa"
+
+const TempGrid = () => {
+  return (
+    <div className="card-container">
+      <div className="card">
+        <div className="card-icon">
+          <FaArrowDown />
+          <small>min</small>
+        </div>
+        <h2>32 °C</h2>
+      </div>
+
+      <div className="card">
+        <div className="card-icon">
+          <FaArrowDown />
+          <small>min</small>
+        </div>
+        <h2>32 °C</h2>
+      </div>
+
+      <div className="card">
+        <div className="card-icon">
+          <FaArrowDown />
+          <small>min</small>
+        </div>
+        <h2>32 °C</h2>
+      </div>
+     
+    </div>
+  )
+}
+
+export default TempGrid
