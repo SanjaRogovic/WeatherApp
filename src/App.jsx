bde786841081c8665246d0c42b71bfd393992af6
Sanja <74556703+SanjaRogovic@users.noreply.github.com>
2023-11-26 19:45:05 +0100

    simple app done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from "react";
 // import { ToastContainer, toast } from "react-toastify";
 // import "react-toastify/dist/ReactToastify.css";
-
-// import Loader from "./components/Loader";
+import Loader from "./components/Loader";
 import { getWeatherData } from "./components/GetData";
-// import { DateTime } from "luxon";
-
+import { DateTime } from "luxon";
 
 function App() {
   const [weather, setWeather] = useState(null);
@@ -13,8 +11,8 @@ function App() {
   const [loading, setLoading] = useState(false);
   const [localTime, setLocalTime] = useState("");
 
-  // const now = DateTime.now();
-  // console.log(localTime);
+  const now = DateTime.now();
+  console.log(localTime);
 
   const fetchWeather = async () => {
     try {
@@ -24,7 +22,6 @@ function App() {
       setWeather(data);
 
       if (!city || city === "") return;
-
     } catch (error) {
       console.log("ERROR FETCHING DATA", error.message);
     } finally {
@@ -32,71 +29,80 @@ function App() {
     }
   };
 
-
-
   useEffect(() => {
     fetchWeather();
+
+    setLocalTime(now.toLocaleString(DateTime.DATETIME_MED));
+
+    // const message = city ? city : null;
+    // toast.info("Fetching weather for " + message);
   }, []);
 
-  
 
   return (
     <>
       <h1>Weather Forecast</h1>
 
-      {!weather ? (
+      {!weather || loading ? (
         <div className="box">
-        <input
-          type="text"
-          className="input"
-          value={city}
-          placeholder="search for location ..."
-          onChange={(e) => setCity(e.target.value)}
-        />
-        <button type="submit" className="button" onClick={() => fetchWeather()}>
-          SEARCH
-        </button>
-      </div>
+          <input
+            type="text"
+            className="input"
+            value={city}
+            placeholder="search for location ..."
+            onChange={(e) => setCity(e.target.value)}
+            required
+          />
+          <button
+            type="submit"
+            className="button"
+            onClick={() => fetchWeather()}
+          >
+            SEARCH
+          </button>
+        </div>
       ) : (
         <>
           {weather !== null ? (
             <div className="box">
               <div className="inner-box">
-              <div className="location">
-                <h3>
-                  <i className="fa fa-street-view"></i>
-                  {weather.name} | {weather.sys.country}
-                </h3>
-              </div>
-              <div className="weather-icon">
-                <img
-                  src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
-                  alt="imgicon"
-                />
-              </div>
-              <h4>{weather.weather[0].main}</h4>
-              <div className="temperature">
-                <p>
-                  {parseFloat(weather.main.temp - 273.15).toFixed(1)}&deg;C
-                </p>
-              </div>
-              
-              <div className="temperature-range">
-                <h6>
-                  Min: {parseFloat(weather.main.temp_min - 273.15).toFixed(1)}
-                  &deg;C || Max:{" "}
-                  {parseFloat(weather.main.temp_max - 273.15).toFixed(1)}
-                  &deg;C || Humidity: {weather.main.humidity}%
-                </h6>
-              </div>
+                <div className="location">
+                  <h3>
+                    <i className="fa fa-street-view"></i>
+                    {weather.name} | {weather.sys.country}
+                  </h3>
+                  <p className="time">{localTime}</p>
+                </div>
+                <div className="weather-icon">
+                  <img
+                    src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
+                    alt="imgicon"
+                  />
+                </div>
+                <h4>{weather.weather[0].main}</h4>
+                <div className="temperature">
+                  <p>
+                    {parseFloat(weather.main.temp - 273.15).toFixed(1)} &deg;C
+                  </p>
+                </div>
 
-
+                <div className="temperature-range">
+                  <h6>
+                    Min: {parseFloat(weather.main.temp_min - 273.15).toFixed(1)}
+                    &deg;C || Max:{" "}
+                    {parseFloat(weather.main.temp_max - 273.15).toFixed(1)}
+                    &deg;C || Humidity: {weather.main.humidity}%
+                  </h6>
+                </div>
               </div>
-              
             </div>
-          ) : null}
+          ) : (
+            <Loader />
+          )}
         </>
       )}
+
+      {/* <ToastContainer autoClose={5000} theme="colored" newestOnTop={true} /> */}
     </>
   );
 }
